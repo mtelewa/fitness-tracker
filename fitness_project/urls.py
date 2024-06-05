25a9ac7a227f42cc7be73e706cee233e2cfe2d23
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-05 15:55:50 +0000

    add nutrition_guide app and link it to the project

--- fitness_project/urls.py
+++ fitness_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from nutrition_guide.views import index
 
 urlpatterns = [
+    path('nutrition_guide/', index, name='nutrition_guide'),
     path('admin/', admin.site.urls),
 ]
