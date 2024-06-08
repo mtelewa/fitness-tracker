1a3100e2863a8ffafaa5d702ced88e10f187a4bd
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-08 08:59:01 +0000

    change project name from fitness_project to fitness_tracker

--- fitness_tracker/wsgi.py
+++ fitness_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fitness_project project.
+WSGI config for fitness_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_tracker.settings')
 
 application = get_wsgi_application()
