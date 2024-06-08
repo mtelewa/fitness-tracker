5855855d4ce3894831737d20ca3007a99d32a182
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-08 08:59:01 +0000

    change project name from fitness_project to fitness_tracker

--- fitness_tracker/urls.py
+++ fitness_tracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fitness_project project.
+URL configuration for fitness_tracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from nutrition_guide.views import index 
 
 urlpatterns = [
-    path('nutrition_guide/', index, name='nutrition_guide'),
+    path("fitness_tracker/", include("nutrition_guide.urls"), name="nutrition_guide-urls"),
     path('admin/', admin.site.urls),
 ]
