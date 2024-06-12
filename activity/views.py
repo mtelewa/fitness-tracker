a0ac01214368dac1fa86ec1d07d687d2d1a2d406
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-12 19:50:38 +0000

    add test function in dashboard app views

--- activity/views.py
+++ activity/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Activity
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse('Hi. I am working!')
+
+
 def index(request):
-    return HttpResponse('Hi. I am working!')
+    """
+    Display an individual :model:`dashboard.Activity`.
+
+    **Context**
+
+    ``activity``
+        An instance of :model:`dashboard.Activity`.
+
+    **Template:**
+
+    :template:`activity/index.html`
+    """
+
+    activity = Activity.objects.all()
+
+    return render(
+        request,
+        "activity/index.html",
+        {
+            "activity": activity,
+        }
+    )
