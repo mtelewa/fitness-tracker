48b172f0af8a7e54b3d0ac09b7a138117f9c40de
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-17 21:47:46 +0000

    update dashboard custom models and make migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,18 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+STARS = ((1, "Very Poor"), (2, "Poor"), (3, "Good"), (4, "Very Good"), (5, "Excellent"))
+
 class Activity(models.Model):
     activity_type = models.CharField(max_length=200, unique=True)
-    user = models.CharField(max_length=200, unique=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_activity"
+        )
     distance = models.FloatField()
     duration = models.FloatField()
-    registered_on = models.DateTimeField(auto_now_add=True)
-    calories_burnt = models.IntegerField()
+    activity_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         """
         The ordering option adds metadata to the model about
         the default order in which the list of activities is displayed
         """
-        ordering = ["registered_on"]
+        ordering = ["activity_on"]
+
+
+class Nutrition(models.Model):
+    food_item = models.CharField(max_length=200, unique=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_nutrition"
+        )
+    nutrition_on = models.DateTimeField(auto_now_add=True)
+
+
+class Feedback(models.Model):
+    message = models.TextField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_review"
+        )
+    stars = models.IntegerField(choices=STARS, default=0)
+    review = models.TextField()
+    approved = models.BooleanField(default=False)
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_profile"
+        )
+    height = models.IntegerField()
+    weight = models.FloatField()
+    profile_image = CloudinaryField('image', default="placeholder")
