822054cecd510cb1abf1320ce5f853af71fca438
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-19 11:08:26 +0000

    remove dateutil to deploy on heroku

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,69 +7,70 @@ import datetime
 
 # Create your views here.
 
-# def index(request):
-#     return HttpResponse('Hi. I am working!')
-
-
 def index(request):
-    """
-    Display an individual :model:`dashboard.Activity`.
+    return HttpResponse('Hi. I am working!')
 
-    **Context**
 
-    ``activity``
-        An instance of :model:`dashboard.Activity`.
+# def index(request):
+#     """
+#     Display an individual :model:`dashboard.Activity`.
 
-    **Template:**
+#     **Context**
 
-    :template:`activity/index.html`
-    """
+#     ``activity``
+#         An instance of :model:`dashboard.Activity`.
 
-    # activity = Activity.objects.all()
+#     **Template:**
 
-    user = Profile.objects.get(user=request.user.id)
-    weight = user.weight
-    height = user.height
-    birthdate = user.birthdate
+#     :template:`activity/index.html`
+#     """
 
-    # Body Mass Index (BMI)
-    bmi_unrounded = weight / (height/100)**2    # kg/m2
-    bmi = np.round(bmi_unrounded, 2)
+#     # activity = Activity.objects.all()
 
-    if bmi < 16:
-        classification = 'Severe Thinness'
-    elif bmi >= 16 and bmi < 17:
-        classification = 'Moderate Thinness'
-    elif bmi >= 17 and bmi < 18.5:
-        classification = 'Mild Thinness'
-    elif bmi >= 18.5 and bmi < 25:
-        classification = 'Normal'
-    elif bmi >= 25 and bmi < 30:
-        classification = 'Overweight'
-    elif bmi >= 30 and bmi < 35:
-        classification = 'Obese Class I'
-    elif bmi >=35 and bmi < 40:
-        classification = 'Obese Class II'
-    else:
-        classification = 'Obese Class III'
+#     user = Profile.objects.get(user=request.user.id)
+#     print(user)
+#     weight = user.weight
+#     height = user.height
+#     birthdate = user.birthdate
 
-    # Age
-    # today = datetime.date.today()
-    # age = relativedelta(today, birthdate).years
+#     # Body Mass Index (BMI)
+#     bmi_unrounded = weight / (height/100)**2    # kg/m2
+#     bmi = np.round(bmi_unrounded, 2)
+
+#     if bmi < 16:
+#         classification = 'Severe Thinness'
+#     elif bmi >= 16 and bmi < 17:
+#         classification = 'Moderate Thinness'
+#     elif bmi >= 17 and bmi < 18.5:
+#         classification = 'Mild Thinness'
+#     elif bmi >= 18.5 and bmi < 25:
+#         classification = 'Normal'
+#     elif bmi >= 25 and bmi < 30:
+#         classification = 'Overweight'
+#     elif bmi >= 30 and bmi < 35:
+#         classification = 'Obese Class I'
+#     elif bmi >=35 and bmi < 40:
+#         classification = 'Obese Class II'
+#     else:
+#         classification = 'Obese Class III'
+
+#     # Age
+#     # today = datetime.date.today()
+#     # age = relativedelta(today, birthdate).years
     
-    # Basal Metabolic Rate (BMR)
-    # Revised Harris-Benedict Equation (for Men - just for illustration)
-    # bmr = np.round(13.397 * weight + 4.799 * height - 5.677 * age + 88.362)
+#     # Basal Metabolic Rate (BMR)
+#     # Revised Harris-Benedict Equation (for Men - just for illustration)
+#     # bmr = np.round(13.397 * weight + 4.799 * height - 5.677 * age + 88.362)
 
-    return render(
-        request,
-        "dashboard/index.html",
-        {
-            'bmi': bmi,
-            'classification': classification,
-            'weight': weight,
-            'height': height,
-            # 'age': age,
-            # 'bmr': bmr,
-        }
-    )
+#     return render(
+#         request,
+#         "dashboard/index.html",
+#         {
+#             'bmi': bmi,
+#             'classification': classification,
+#             'weight': weight,
+#             'height': height,
+#             # 'age': age,
+#             # 'bmr': bmr,
+#         }
+#     )
