b3d70bd5eaab4a6c16437ff0709893230562ea0a
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-20 19:45:40 +0000

    set the date format for the birthdate in forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,6 +2,8 @@ from .models import Profile
 from django import forms
 
 class MetricsForm(forms.ModelForm):
+    birthdate = forms.DateField(input_formats=['%d-%m-%Y'], initial='dd-mm-yyyy')
+
     class Meta:
         model = Profile
         fields = ('height','weight','profile_image','birthdate')
