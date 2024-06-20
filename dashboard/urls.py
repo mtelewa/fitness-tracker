0555a6d567b5daf1373006eff828709f6affe6c5
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-20 19:35:40 +0000

    update dashboard function in views and update the url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,6 @@
-from . import views
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.dashboard, name='home')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Activity, Profile
-import numpy as np
+from django.contrib import messages
 from datetime import date, timedelta
-# from dateutil.relativedelta import relativedelta
+import numpy as np
+from .models import Activity, Profile
+from .forms import MetricsForm
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ from datetime import date, timedelta
 #     return HttpResponse('Hi. I am working!')
 
 
-def index(request):
+def dashboard(request):
     """
     Display an individual :model:`dashboard.Activity`.
 
@@ -22,53 +23,96 @@ def index(request):
 
     **Template:**
 
-    :template:`activity/index.html`
+    :template:`dashboard/index.html`
     """
 
-    # # activity = Activity.objects.all()
+    if request.user.is_authenticated:
 
-    user = Profile.objects.get(user=request.user)
-    weight = user.weight
-    height = user.height
-    birthdate = user.birthdate
+        # # activity = Activity.objects.all()
+        # Update foreign key
+        p = Profile.objects.get(pk=1)
+        Profile.objects.all().update(user=p)
 
-    # Body Mass Index (BMI)
-    bmi_unrounded = weight / (height/100)**2    # kg/m2
-    bmi = np.round(bmi_unrounded, 2)
+        queryset = Profile.objects.filter(user=request.user).latest('updated_on')
 
-    if bmi < 16:
-        classification = 'Severe Thinness'
-    elif bmi >= 16 and bmi < 17:
-        classification = 'Moderate Thinness'
-    elif bmi >= 17 and bmi < 18.5:
-        classification = 'Mild Thinness'
-    elif bmi >= 18.5 and bmi < 25:
-        classification = 'Normal'
-    elif bmi >= 25 and bmi < 30:
-        classification = 'Overweight'
-    elif bmi >= 30 and bmi < 35:
-        classification = 'Obese Class I'
-    elif bmi >=35 and bmi < 40:
-        classification = 'Obese Class II'
-    else:
-        classification = 'Obese Class III'
+        # user = Profile.objects.get(user=request.user)
+        weight = queryset.weight
+        height = queryset.height
+        birthdate = queryset.birthdate
 
-    # Age
-    age = (date.today() - birthdate) // timedelta(days=365.2425)
+        # If the user updates the values
+        metrics_form = MetricsForm()
+
+        if request.method == "POST":
+            metrics_form = MetricsForm(data=request.POST)
+
+            if metrics_form.is_valid():
+                # profile = metrics_form.save(commit=False)
+                weight = metrics_form.cleaned_data.get('weight')
+                height = metrics_form.cleaned_data.get('height')
+                birthdate = metrics_form.cleaned_data.get('birthdate')
+
+                profile = Profile(height=height, weight=weight, birthdate=birthdate)
+                print('form is valid')
+                profile.save()
+
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    'Your values have been updated!'
+                    )
+
+        # Body Mass Index (BMI)
+        bmi_unrounded = weight / (height/100)**2    # kg/m2
+        bmi = np.round(bmi_unrounded, 2)
+
+        if bmi < 16:
+            classification = 'Severe Thinness'
+        elif bmi >= 16 and bmi < 17:
+            classification = 'Moderate Thinness'
+        elif bmi >= 17 and bmi < 18.5:
+            classification = 'Mild Thinness'
+        elif bmi >= 18.5 and bmi < 25:
+            classification = 'Normal'
+        elif bmi >= 25 and bmi < 30:
+            classification = 'Overweight'
+        elif bmi >= 30 and bmi < 35:
+            classification = 'Obese Class I'
+        elif bmi >=35 and bmi < 40:
+            classification = 'Obese Class II'
+        else:
+            classification = 'Obese Class III'
+
+        # Age
+        age = (date.today() - birthdate) // timedelta(days=365.2425)
+        
+        # Basal Metabolic Rate (BMR)
+        # Revised Harris-Benedict Equation (for Men - just for illustration)
+        bmr = np.round(13.397 * weight + 4.799 * height - 5.677 * age + 88.362)
+
+        rec_bmi = 24
+        rec_weight =  np.round(rec_bmi * (height/100)**2, 1)
+
+        # target_weight = 
+
+
+        return render(
+            request,
+            "dashboard/index.html",
+            {
+                'bmi': bmi,
+                'bmr': bmr,
+                'classification': classification,
+                'weight': weight,
+                'height': height,
+                'age': age,
+                'metrics_form': metrics_form,
+                'rec_bmi': rec_bmi,
+                'rec_weight': rec_weight,
+            }
+        )
     
-    # Basal Metabolic Rate (BMR)
-    # Revised Harris-Benedict Equation (for Men - just for illustration)
-    bmr = np.round(13.397 * weight + 4.799 * height - 5.677 * age + 88.362)
-
-    return render(
-        request,
-        "dashboard/index.html",
-        {
-            'bmi': bmi,
-            'classification': classification,
-            'weight': weight,
-            'height': height,
-            'age': age,
-            'bmr': bmr,
-        }
-    )
+    else:
+        return render(
+            request,
+            "dashboard/index.html",
+        )
