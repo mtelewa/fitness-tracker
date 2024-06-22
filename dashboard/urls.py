bc1fd1cc5840f827b9cfac0822fa25b4a12e7d40
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-22 20:17:59 +0000

    update urls for the dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='home')
+    path('', views.dashboard, name='home'),
+    path('profile/', views.profile, name='profile'),
 ]
