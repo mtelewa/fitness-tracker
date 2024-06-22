b888c3f97f827d9bbf836c94a8f5db928365a99f
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-22 20:25:54 +0000

    add form helper to metrics form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,9 +1,30 @@
-from .models import Profile
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Button
+from django.shortcuts import reverse
+from .models import Profile
 
 class MetricsForm(forms.ModelForm):
-    birthdate = forms.DateField(input_formats=['%d-%m-%Y'], initial='dd-mm-yyyy')
+    # birthdate = forms.DateField(input_formats=['%d-%m-%Y'], initial='dd-mm-yyyy')
+    weight_target = forms.FloatField(
+        label = "Target Weight",
+        required = True,
+        )
 
     class Meta:
         model = Profile
-        fields = ('height','weight','profile_image','birthdate')
+        fields = ('weight',)
+
+    # helper form
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        # self.helper.form_class = 'form-inline'
+        # self.helper.field_template = 'bootstrap5/layout/inline_field.html'
+        
+        self.helper.add_input(Submit('submit', 'Submit', css_class='btn-success'))
+        self.helper.add_input(Button('cancel', 'Cancel', css_class='btn-secondary',
+            onclick="window.location.href = '{}';".format(reverse('home'))))
