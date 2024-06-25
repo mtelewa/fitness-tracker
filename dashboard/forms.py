c3f0e39e4b7224697077e3d1ae1e9213a77fc9fb
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-25 09:06:58 +0000

    add ProfileForm

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,7 +5,7 @@ from django.shortcuts import reverse
 from .models import Profile
 
 class MetricsForm(forms.ModelForm):
-    # birthdate = forms.DateField(input_formats=['%d-%m-%Y'], initial='dd-mm-yyyy')
+
     weight_target = forms.FloatField(
         label = "Target Weight",
         required = True,
@@ -19,12 +19,27 @@ class MetricsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-
-        self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        # self.helper.form_class = 'form-inline'
-        # self.helper.field_template = 'bootstrap5/layout/inline_field.html'
+        self.helper.form_class = 'forms'
         
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn-success'))
         self.helper.add_input(Button('cancel', 'Cancel', css_class='btn-secondary',
             onclick="window.location.href = '{}';".format(reverse('home'))))
+
+
+class ProfileForm(forms.ModelForm):
+    birthdate = forms.DateField(input_formats=['%d-%m-%Y'])
+
+    class Meta:
+        model = Profile
+        fields = ('height', 'profile_image', )
+
+    # helper form
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        
+        self.helper.add_input(Submit('submit', 'Submit', css_class='btn-success'))
+        self.helper.add_input(Button('cancel', 'Cancel', css_class='btn-secondary',
+            onclick="window.location.href = '{}';".format(reverse('profile'))))
