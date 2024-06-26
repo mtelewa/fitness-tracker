1bc3ea9a7c10943b3c44dd1e39e0443e33a24bbc
Mohamed Elewa <m.tarek@live.co.uk>
2024-06-26 12:50:22 +0000

    update function name in urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='home'),
+    path('', views.metrics, name='home'),
     path('profile/', views.profile, name='profile'),
     path('calendar/', views.calendar, name='calendar'),
 ]
