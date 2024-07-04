a3b3b2eb2ea8807f79eae50ae549e116a4159bd1
Mohamed Elewa <m.tarek@live.co.uk>
2024-07-04 21:43:37 +0000

    adregisterutrition t model to admin board

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -8,10 +8,15 @@ from .models import *
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('activity_type',)
-
+    list_display = ('activity_type', 'duration', 'calories_burnt', 'activity_on',)
+    ordering = ('-activity_on',)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('weight', 'height', 'birthdate', 'updated_on',)
     ordering = ('-updated_on',)
+
+@admin.register(Nutrition)
+class NutritionAdmin(admin.ModelAdmin):
+    list_display = ('food_item', 'calories_intake',)
+    ordering = ('-nutrition_on',)
